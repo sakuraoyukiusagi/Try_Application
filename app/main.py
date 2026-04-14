ed46205856755088be7fe5f6a5407da9bd8b2703
sakuraoyukiusagi <s3579515@gmail.com>
2026-04-14 15:00:31 +0800

    add app

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "version": os.getenv("APP_VERSION", "v2.0"),
+        "status": "ok",
+    }
